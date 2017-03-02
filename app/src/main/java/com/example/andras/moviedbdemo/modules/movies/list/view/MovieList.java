45c30a252eaead8e878b0322fc12c7b2bc87d93f
Andras Nemeth <guni82hu@gmail.com>
2017-03-02 14:17:39 +0100

    initial commit

---
+++ app/src/main/java/com/example/andras/moviedbdemo/modules/movies/list/view/MovieList.java
@@ -0,0 +1,15 @@
+package com.example.andras.moviedbdemo.modules.movies.list.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andras.moviedbdemo.R;
+
+public class MovieList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
